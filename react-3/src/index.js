479b0dcafc30584caa74284d42f2fa8b6cfd0fee
peeravali0110 <peeravali0110@gmail.com>
2023-01-21 22:40:09 +0530

    react

---
+++ react-3/src/index.js
@@ -0,0 +1,5 @@
+import ReactDom from 'react-dom'
+
+ReactDom.render("hello good morning my self i am syed peera vali i have completed my bachelors of engineering in the stream of eee from muffakham jah college of engineering and technlogy",document.getElementById("abc"));
+
+
